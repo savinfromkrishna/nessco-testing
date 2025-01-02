e1e65d95aaace64a4b001d9d3d75f0bdaeb802f4
kanha-9770 <app3.nessco@gmail.com>
2025-01-02 10:15:06 +0530

    testing

--- src/app/api/environment.ts
+++ src/app/api/environment.ts
@@ -2,17 +2,15 @@ import { headers } from "next/headers";
 
 export function getBaseUrl(): string {
   const headerMap = headers();
+  console.log("Headers:", headerMap);
 
-  // Fallbacks for missing headers
   const host = headerMap.get("host") || "localhost:3000";
   const protocol =
     headerMap.get("x-forwarded-proto") || (host.includes("localhost") ? "http" : "https");
   const urlPath = headerMap.get("x-next-url-path") || "/";
   const query = headerMap.get("x-next-url-query") || "";
 
-  // Properly encode the query
   const formattedQuery = query ? `?${query}` : "";
 
-  // Construct the base URL
   return `${protocol}://${host}${urlPath}${formattedQuery}`;
 }
